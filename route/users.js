9762e03b52220b31e224d03842e0b64533b720c1
Koushik Roy <koushikroy.jis@gmail.com>
2018-09-30 06:41:26 +0530

    User route added

--- route/users.js
+++ route/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const route = express.Router();
 route.use(express.json());
 
+
 route.get('/', (req, res)=>{
     res.status(200).send('Hi')
-})
+});
+
+module.exports = route;
